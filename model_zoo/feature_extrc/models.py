0d5682ffa9b242b273b02c1963fbc3eccc585155
marziehoghbaie <marzieh.oghbaie@yahoo.com>
2025-03-28 09:56:55 +0100

    Update models.py

--- model_zoo/feature_extrc/models.py
+++ model_zoo/feature_extrc/models.py
@@ -63,7 +63,6 @@ class ViT_VaR(nn.Module):
                                   num_patch_new,
                                   mode=self.interpolation_type).transpose(-2, -1)
             self.temporal_pos_encodings = nn.Parameter(tmp)
-            #trunc_normal_(self.temporal_pos_encodings, std=.02)
 
         return self.temporal_pos_encodings
 
